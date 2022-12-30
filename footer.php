609dbbbf4f585369a5f1a036ce69ef18cce756e9
mandarin <hoojunee@gmail.com>
2022-12-30 14:39:26 +0900

    hj_footer_22.12.30
    footer 시작함. 위,아래 color 추가 및 내부 desc까지 생성함

--- footer.php
+++ footer.php
@@ -1,12 +1,41 @@
+    </main>
+    </body>
+    <footer>
+      <section class="hj_high_section"></section>
+      <section class="hj_low_section">
+        <div class="hj_footer_box">
+          <div class="hj_footer_menu">
+            <ul>
+              <!-- gnb 메뉴시작 -->
+              <li>
+                <a href="./about.php">
+                  About us
+                </a>
+              </li><!-- 연혁 -->
+              <li>
+                <a href="./coffee.php">
+                  coffee
+                </a>
+              </li><!--  상품소개 -->
+              <li>
+                <a href="./cafes.php">
+                  cafes
+                </a>
+              </li><!-- 가게위치 -->
+            </ul>
+          </div>
+          <div class="hj_footer_desc">
+            <figure>
+              <img src="./HJ/img/logo.png" alt>
+            </figure>
+            <ul>
+              <li>bluebottle coffee company</li>
+              <li>Copyright 2022 Bluebottle Team All Right Reserved.</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </footer>
+    </body>
 
-
-    </main>      
-  </body>
-  <footer>
-    <!-- footer  시작 -->
-    <hr>
-    footer
-    <!-- footer  끝 -->
-  </footer>
-</body>
-</html>
+    </html>
