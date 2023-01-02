97d33be2146b2650c96ce7860937b49aa7333401
Choi-Mele <cikth0810@gmail.com>
2023-01-02 18:00:58 +0900

    ik style fix

--- index.php
+++ index.php
@@ -17,7 +17,12 @@
     <svg class="ik_wave2" viewBox="0 0 1200 196"  preserveAspectRatio="none">
       <path d="M0,66.4c0,0,43.9-19,94.2,0c42.3,29,57.9,35,115.4,40c52.6-3,113.1,16,152.1,37s64.3,8,113.1,6c43.9,3,68.7,17.9,111.1,41	c31.2-31,68.2-51,100.4-51c32.2,3,84.8,13,120.9-4c36.1-17,79.9-41,121.9-46c53.6-10,85.8-4,134.5,6c56.5,0,136.5-35,136.5-35v140.1	H0V66.4z" />
     </svg>
-  </section>
+  </section><!-- ik_main1 -->
+
+  <svg class="ik_background_flow" viewBox="0 0 1920 3500" perspectiveRatio="none">
+	  <path fill="none" d="M110.46,248.33c-299.01,221.64-229,567-4,721c424,259,941,361,1397,400c397.87,34.03,506,243.68,506,243.68 s0,512.32,0,791.32c-104-252-242-492-817-708c-376-187-830-346-1139-594s-323-555-197-749c246-379,1000-362,1000-362 C511.46,39.33,338.46,79.33,110.46,248.33z"/>
+  </svg>
+  
   <section class="ik_main_2">
     <figure class="ik_mug">
       <img src="./img/object/mug01.png" alt="">
@@ -27,6 +32,15 @@
         <img src="./img/main_section/1-1.png" alt="">
         <img src="./img/main_section/1-2.png" alt="">
         <img src="./img/main_section/1-3.png" alt="">
+        <p class="ik_textbox ik_textbox1">
+          We Are a
+        </p>
+        <p class="ik_textbox ik_textbox2">
+          Specialty
+        </p>
+        <p class="ik_textbox ik_textbox3">
+          Coffee Company
+        </p>
       </div>
       <div class="ik_motion2"></div>
     </div>
