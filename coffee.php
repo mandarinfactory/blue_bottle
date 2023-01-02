8e80f1e51833a7d76ca5cc49dfd34514191d2e58
mandarin <hoojunee@gmail.com>
2023-01-02 22:33:17 +0900

    footer_23.01.02
    footer design, SVG animation & color, height modified

--- coffee.php
+++ coffee.php
@@ -1,9 +1,16 @@
 <?php include 'sub-header.php' ?>
 
-  <!-- main  아래 본문시작 -->
-  
-    상품소개
+<!-- main  아래 본문시작 -->
 
-  <!-- main  위 본문 끝 -->
+상품소개
+<br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br>
+
+<!-- main  위 본문 끝 -->
 
 <?php include 'footer.php' ?>
--- footer.php
+++ footer.php
@@ -1,11 +1,53 @@
+    </main>
+    <footer>
+      <section class="hj_footer_section">
+        <figure class="high_wave">
+          <svg viewBox="0 0 1920 148" preserveAspectRatio="none">
+            <path class="st0" d="M1920,101H0.5V9v0.33c0,0,429.58,64.97,675.13,62.21c245.56-2.76,324.87-49.2,601.55-60.24 S1909.62,53.85,1920,53.85V101z" />
+          </svg>
+        </figure>
+        <figure class="middle_wave">
+          <svg viewBox="0 0 1920 148" preserveAspectRatio="none">
+            <path class="st1" d="M1920,101H0.5V9v0.33c0,0,429.58,64.97,675.13,62.21c245.56-2.76,324.87-49.2,601.55-60.24 S1909.62,53.85,1920,53.85V101z" />
+          </svg>
+        </figure>
+        <figure class="low_wave">
+          <svg viewBox="0 0 1920 148" preserveAspectRatio="none">
+            <path class="st2" d="M1920,101H0.5V9v0.33c0,0,429.58,64.97,675.13,62.21c245.56-2.76,324.87-49.2,601.55-60.24 S1909.62,53.85,1920,53.85V101z" />
+          </svg>
+        </figure>
+        <div class="hj_footer_box">
+          <div class="hj_footer_menu">
+            <ul>
+              <li>
+                <a href="./about.php">
+                  About us
+                </a>
+              </li><!-- 연혁 -->
+              <li>
+                <a href="./coffee.php">
+                  coffee
+                </a>
+              </li><!--  상품소개 -->
+              <li>
+                <a href="./cafes.php">
+                  cafes
+                </a>
+              </li><!-- 가게위치 -->
+            </ul>
+          </div>
+          <div class="hj_footer_desc">
+            <figure>
+              <img src="./HJ/img/logo.png" alt="">
+            </figure>
+            <ul>
+              <li>bluebottle coffee company</li>
+              <li>Copyright 2022 Bluebottle Team All Right Reserved.</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </footer>
+    </body>
 
-
-    </main>      
-  <footer>
-    <!-- footer  시작 -->
-    <hr>
-    footer
-    <!-- footer  끝 -->
-  </footer>
-</body>
-</html>
+    </html>
