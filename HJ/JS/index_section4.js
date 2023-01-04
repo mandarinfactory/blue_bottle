b60109edb1c111bde1872a0123df2c7a61b33643
mandarin <hoojunee@gmail.com>
2023-01-04 17:56:19 +0900

    index_main-section4
    전체적인 design 복구 및 JS 파일 복구함.....

--- HJ/JS/index_section4.js
+++ HJ/JS/index_section4.js
@@ -1,10 +1,12 @@
-const cafeImageArr = [
-    "location_blue1", "location_blue2", "location_blue3", "location_blue4", "location_blue5",
-    "location_blue6", "location_blue7", "location_blue8", "location_blue9", "location_blue10"
-]//cafeImageArr
-
-document.querySelector('.hj_cafes_area a').addEventListener('click', e => {
-    e.preventDefault()
-    let hjAnchorData = e.target.getAttribute('data-num')  
-    console.log(hjAnchorData);   
-})//hjAnchorData
+$(function () {
+    const cafeImageArr = [
+        "location_blue1", "location_blue2", "location_blue3", "location_blue4", "location_blue5",
+        "location_blue6", "location_blue7", "location_blue8", "location_blue9", "location_blue10"
+    ]//cafeImageArr
+    $('.hj_cafes_area a').mouseenter(function () {
+        let anchorData = parseInt($(this).attr('data-num'))
+        if (anchorData === cafeImageArr.indexOf(cafeImageArr[anchorData])) {
+            $('.hj_cafes_photo img').attr("src", "./img/location/" + cafeImageArr[anchorData] + ".jpg");
+        }//if
+    })//mouseenter
+})//document.ready
