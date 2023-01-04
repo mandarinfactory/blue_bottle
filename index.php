53fdd3299ca9a706fb0d94a3d0166b1a24dd8a46
mandarin <hoojunee@gmail.com>
2023-01-04 17:36:23 +0900

    pull && merge

--- index.php
+++ index.php
@@ -7,45 +7,46 @@
 <!-- CSS link -->
 
 <!-- main  아래 본문시작 -->
+<script src="./HJ/JS/index_section4.js"></script>
 <main>
   <section class="ik_main_1">
     <video class="ik_background_video" src="http://cameleon89.synology.me/blue_bottle/video/bluebottle.mp4" autoplay muted loop>
-      </video>
-      <div class="ik_title_box">
-        <h2 class="ik_title">blue bottle</h2>
-        <p>Another Level of Coffee</p>
-      </div>
-      
-      <svg class="ik_wave3" viewBox="0 0 1200 196" preserveAspectRatio="none">
-        <path d="M0,66.4c0,0,43.9-19,94.2,0c42.3,29,57.9,35,115.4,40c52.6-3,113.1,16,152.1,37s64.3,8,113.1,6c43.9,3,68.7,17.9,111.1,41	c31.2-31,68.2-51,100.4-51c32.2,3,84.8,13,120.9-4c36.1-17,79.9-41,121.9-46c53.6-10,85.8-4,134.5,6c56.5,0,136.5-35,136.5-35v140.1	H0V66.4z" />
-      </svg>
-      <svg class="ik_wave1" viewBox="0 0 1200 196" preserveAspectRatio="none">
-        <path d="M0,66.4c0,0,43.9-19,94.2,0c42.3,29,57.9,35,115.4,40c52.6-3,113.1,16,152.1,37s64.3,8,113.1,6c43.9,3,68.7,17.9,111.1,41	c31.2-31,68.2-51,100.4-51c32.2,3,84.8,13,120.9-4c36.1-17,79.9-41,121.9-46c53.6-10,85.8-4,134.5,6c56.5,0,136.5-35,136.5-35v140.1	H0V66.4z" />
-      </svg>
-      <svg class="ik_wave2" viewBox="0 0 1200 196" preserveAspectRatio="none">
-        <path d="M0,66.4c0,0,43.9-19,94.2,0c42.3,29,57.9,35,115.4,40c52.6-3,113.1,16,152.1,37s64.3,8,113.1,6c43.9,3,68.7,17.9,111.1,41	c31.2-31,68.2-51,100.4-51c32.2,3,84.8,13,120.9-4c36.1-17,79.9-41,121.9-46c53.6-10,85.8-4,134.5,6c56.5,0,136.5-35,136.5-35v140.1	H0V66.4z" />
-      </svg>
-    </section><!-- ik_main1 -->
-    
-    <section class="ik_middle">
-      <div class="ik_main_2">
-        <figure class="ik_mug">
-          <img src="./img/object/mug01.png" alt="">
-        </figure>
-        <div class="ik_motion_box">
-          <div class="ik_motion1">
-            <img src="./img/main_section/1-1.png" alt="">
-            <img src="./img/main_section/1-2.png" alt="">
-            <img src="./img/main_section/1-3.png" alt="">
-            <p class="ik_textbox ik_textbox1">
-              We Are a
-            </p>
-            <p class="ik_textbox ik_textbox2">
-              Specialty
-            </p>
-            <p class="ik_textbox ik_textbox3">
-              Coffee Company
-            </p>
+    </video>
+    <div class="ik_title_box">
+      <h2 class="ik_title">blue bottle</h2>
+      <p>Another Level of Coffee</p>
+    </div>
+
+    <svg class="ik_wave3" viewBox="0 0 1200 196" preserveAspectRatio="none">
+      <path d="M0,66.4c0,0,43.9-19,94.2,0c42.3,29,57.9,35,115.4,40c52.6-3,113.1,16,152.1,37s64.3,8,113.1,6c43.9,3,68.7,17.9,111.1,41	c31.2-31,68.2-51,100.4-51c32.2,3,84.8,13,120.9-4c36.1-17,79.9-41,121.9-46c53.6-10,85.8-4,134.5,6c56.5,0,136.5-35,136.5-35v140.1	H0V66.4z" />
+    </svg>
+    <svg class="ik_wave1" viewBox="0 0 1200 196" preserveAspectRatio="none">
+      <path d="M0,66.4c0,0,43.9-19,94.2,0c42.3,29,57.9,35,115.4,40c52.6-3,113.1,16,152.1,37s64.3,8,113.1,6c43.9,3,68.7,17.9,111.1,41	c31.2-31,68.2-51,100.4-51c32.2,3,84.8,13,120.9-4c36.1-17,79.9-41,121.9-46c53.6-10,85.8-4,134.5,6c56.5,0,136.5-35,136.5-35v140.1	H0V66.4z" />
+    </svg>
+    <svg class="ik_wave2" viewBox="0 0 1200 196" preserveAspectRatio="none">
+      <path d="M0,66.4c0,0,43.9-19,94.2,0c42.3,29,57.9,35,115.4,40c52.6-3,113.1,16,152.1,37s64.3,8,113.1,6c43.9,3,68.7,17.9,111.1,41	c31.2-31,68.2-51,100.4-51c32.2,3,84.8,13,120.9-4c36.1-17,79.9-41,121.9-46c53.6-10,85.8-4,134.5,6c56.5,0,136.5-35,136.5-35v140.1	H0V66.4z" />
+    </svg>
+  </section><!-- ik_main1 -->
+
+  <section class="ik_middle">
+    <div class="ik_main_2">
+      <figure class="ik_mug">
+        <img src="./img/object/mug01.png" alt="">
+      </figure>
+      <div class="ik_motion_box">
+        <div class="ik_motion1">
+          <img src="./img/main_section/1-1.png" alt="">
+          <img src="./img/main_section/1-2.png" alt="">
+          <img src="./img/main_section/1-3.png" alt="">
+          <p class="ik_textbox ik_textbox1">
+            We Are a
+          </p>
+          <p class="ik_textbox ik_textbox2">
+            Specialty
+          </p>
+          <p class="ik_textbox ik_textbox3">
+            Coffee Company
+          </p>
         </div>
         <div class="ik_motion2">
           <img src="./img/main_section/2-1.png" alt="">
@@ -63,10 +64,10 @@
         </div>
       </div><!-- ik_motion_box -->
     </div><!-- ik_main2 parallax-->
-    
+
     <div class="ik_textbox">
       <h3>BlueBottle's Coffee</h3>
-      
+
       <p>Whether blend or single origin,</p>
       <p>We treat all our coffees with equal care.</p>
       <p>For every coffee,</p>
@@ -75,9 +76,9 @@
       <p>with exact flavors to aim for and a scoring</p>
       <p>system to ensure</p>
       <p>we hit the mark everytime.</p>
-      
+
     </div>
-    
+
     <div class="ik_main3">
       <ul>
         <li class="ik_main_list">cold brew</li>
@@ -88,110 +89,6 @@
       </ul>
     </div><!-- ik_main3 slider-->
   </section><!-- ik_middle -->
-  
-  <section class="hj_main_4">
-    <div class="hj_text_slider">
-      <div class="hj_text_track">
-        <div class="content">
-          <svg viewBox="-50 0 1980 450" preserveAspectRatio="none">
-            <defs>
-              <path id="text_curve" class="circular" d="M1920,124.94c-38.84-50-105.14-61-165.76-62c-226.38,10-355.2,270-712.3,284 c-297.42,18-464.43-293.44-799.45-303C70.5,35.5,0,79.94,0,79.94" />
-            </defs>
-            <text>
-              <textPath href="#text_curve">
-                <tspan>
-                  We design artful and engaging spaces to be paired with your favorite coffee.
-                </tspan>
-              </textPath>
-            </text>
-          </svg>
-          <svg viewBox="-50 0 1980 450" preserveAspectRatio="none">
-            <defs>
-              <path id="text_curve" class="circular" d="M1920,124.94c-38.84-50-105.14-61-165.76-62c-226.38,10-355.2,270-712.3,284 c-297.42,18-464.43-293.44-799.45-303C70.5,35.5,0,79.94,0,79.94" />
-            </defs>
-            <text>
-              <textPath href="#text_curve">
-                <tspan>
-                  We design artful and engaging spaces to be paired with your favorite coffee.
-                </tspan>
-              </textPath>
-            </text>
-          </svg>
-          <svg viewBox="-50 0 1980 450" preserveAspectRatio="none">
-            <defs>
-              <path id="text_curve" class="circular" d="M1920,124.94c-38.84-50-105.14-61-165.76-62c-226.38,10-355.2,270-712.3,284 c-297.42,18-464.43-293.44-799.45-303C70.5,35.5,0,79.94,0,79.94" />
-            </defs>
-            <text>
-              <textPath href="#text_curve">
-                <tspan>
-                  We design artful and engaging spaces to be paired with your favorite coffee.
-                </tspan>
-              </textPath>
-            </text>
-          </svg>
-          <svg viewBox="-50 0 1980 450" preserveAspectRatio="none">
-            <defs>
-              <path id="text_curve" class="circular" d="M1920,124.94c-38.84-50-105.14-61-165.76-62c-226.38,10-355.2,270-712.3,284 c-297.42,18-464.43-293.44-799.45-303C70.5,35.5,0,79.94,0,79.94" />
-            </defs>
-            <text>
-              <textPath href="#text_curve">
-                <tspan>
-                  We design artful and engaging spaces to be paired with your favorite coffee.
-                </tspan>
-              </textPath>
-            </text>
-          </svg>
-        </div><!-- content -->
-      </div><!-- hj_text_track -->
-    </div><!-- hj_text_slider -->
-    <figure class="hj_cafes_photo">
-      <img src="" alt="">
-    </figure><!-- hj_cafes_photo -->
-    <div class="hj_cafes_area">
-      <a href="" data-num="0">
-        <img src="./HJ/img/marker_main.jpg" alt="">
-        여의도
-      </a>
-      <a href="" data-num="1">
-        <img src="./HJ/img/marker_main.jpg" alt="">
-        명동
-      </a>
-      <a href="" data-num="2">
-        <img src="./HJ/img/marker_main.jpg" alt="">
-        광화문
-      </a>
-      <a href="" data-num="3">
-        <img src="./HJ/img/marker_main.jpg" alt="">
-        삼청
-      </a>
-      <a href="" data-num="4">
-        <img src="./HJ/img/marker_main.jpg" alt="">
-        삼청한옥
-      </a>
-      <a href="" data-num="5">
-        <img src="./HJ/img/marker_main.jpg" alt="">
-        한남
-      </a>
-      <a href="" data-num="6">
-        <img src="./HJ/img/marker_main.jpg" alt="">
-        성수
-      </a>
-      <a href="" data-num="7">
-        <img src="./HJ/img/marker_main.jpg" alt="">
-        역삼
-      </a>
-      <a href="" data-num="8">
-        <img src="./HJ/img/marker_main.jpg" alt="">
-        압구정
-      </a>
-      <a href="" data-num="9">
-        <img src="./HJ/img/marker_main.jpg" alt="">
-        제주
-      </a>
-    </div><!-- hj_cafes_area -->
-  </section><!-- hj_main_4 -->
-  <script src="./HJ/JS/index_section4.js"></script>
-  <!-- JS link -->
   <!-- main  위 본문 끝 -->
-  
+
   <?php include 'footer.php' ?>
