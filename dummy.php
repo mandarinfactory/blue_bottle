60825d6ef91a058ab7f1ee944bb3fab0b3818b37
Choi-Mele <cikth0810@gmail.com>
2023-01-04 18:26:49 +0900

    ik commit

--- dummy.php
+++ dummy.php
@@ -1,9 +1,33 @@
-<?php include 'dummy-sub-header.php' ?>
+<?php include 'header.php' ?>
+<link rel="stylesheet" href="./css/main/ik_main_1.css">
+<link rel="stylesheet" href="./css/main/ik_main_2.css">
+<link rel="stylesheet" href="./css/main/ik_main_3.css">
+<link rel="stylesheet" href="./css/main/hj_main_4.css">
+<link href="https://fonts.googleapis.com/css2?family=Sacramento&display=swap" rel="stylesheet">
+<!-- CSS link -->
 
-  <!-- main  아래 본문시작 -->
-  
-    dummy.html
+<!-- main  아래 본문시작 -->
+<main>
+
+  <?php include "./main-section1.php" ?>
+
+  <section class="ik_middle">
+    <?php include "./main-section2.php" ?>
+    <div class="ik_textbox">
+      <h3>BlueBottle's Coffee</h3>
+
+      <p>Whether blend or single origin,</p>
+      <p>We treat all our coffees with equal care.</p>
+      <p>For every coffee,</p>
+      <p>We create a custom roast designed for the best</p>
+      <p>expression of that flavor profile.</p>
+      <p>with exact flavors to aim for and a scoring</p>
+      <p>system to ensure</p>
+      <p>we hit the mark everytime.</p>
+    </div>
+    <?php include "./main-section3.php" ?>
+  </section><!-- ik_middle -->
 
   <!-- main  위 본문 끝 -->
-
-<?php include 'dummy-footer.php' ?>
+  
+  <?php include 'footer.php' ?>
