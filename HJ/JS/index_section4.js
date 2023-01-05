5944ae5822c6497968d76c287117f6ab4b34ef1d
mandarin <hoojunee@gmail.com>
2023-01-05 16:33:48 +0900

    bluebottle_23.01.05
    index_section4 SVG, animation, color, 글 수정

--- HJ/JS/index_section4.js
+++ HJ/JS/index_section4.js
@@ -3,21 +3,13 @@ $(function () {
         "location_blue1", "location_blue2", "location_blue3", "location_blue4", "location_blue5",
         "location_blue6", "location_blue7", "location_blue8", "location_blue9", "location_blue10"
     ]//cafeImageArr
+    $('.hj_cafes_photo img').css({ 'display': 'none' })
     $('.hj_cafes_area a').mouseenter(function () {
-        $('.hj_cafes_photo svg').css({'display':'none'})
-        $('.hj_cafes_photo img').css({'display':'block'})
+        $('.hj_cafes_photo svg').css({ 'display': 'none' })
+        $('.hj_cafes_photo img').css({ 'display': 'block' })
         let hj_anchorData = parseInt($(this).attr('data-num'))
         if (hj_anchorData === cafeImageArr.indexOf(cafeImageArr[hj_anchorData])) {
             $('.hj_cafes_photo img').attr("src", "./img/location/" + cafeImageArr[hj_anchorData] + ".jpg");
         }//if
     })//mouseenter
-    $('.hj_cafes_area a').mouseleave(function () {
-        $('.hj_cafes_photo img').css({'display':'none'})
-        $('.hj_cafes_photo svg').css({'display':'block'})
-    })//mouseleave
-
-    let hj_clickAnchorData = $('.hj_cafes_area a').click(function () {
-        $(this).attr('data-num')
-    })//hj_anchorClick 
-    if(hj_clickAnchorData){}
 })//document.ready
--- main-section4.php
+++ main-section4.php
@@ -51,17 +51,34 @@
             </textPath>
           </text>
         </svg>
+        <svg viewBox="0 10 1920 374" preserveAspectRatio="none">
+          <defs>
+            <path id="text_curve" class="circular" d="M1920,124.94c-38.84-50-105.14-61-165.76-62c-226.38,10-355.2,270-712.3,284 c-297.42,18-464.43-293.44-799.45-303C70.5,35.5,0,79.94,0,79.94" />
+          </defs>
+          <text>
+            <textPath href="#text_curve">
+              <tspan>
+                We design artful and engaging spaces to be paired with your favorite coffee.
+              </tspan>
+            </textPath>
+          </text>
+        </svg>
       </div><!-- content -->
     </div><!-- .hj_text_track -->
   </div><!-- ,hj_text_slider -->
+  <figure class="hj_mug">
+    <img class="mug" src="./HJ/img/mug.png" alt="">
+  </figure>
   <figure class="hj_cafes_photo">
     <svg viewBox="0 0 100 100">
       <defs>
-        <path id="circle" d="M 50, 50 m -37, 0 a 37,37 0 1,1 74,0 a 37,37 0 1,1 -74,0" />
+        <path id="circle" d="M 50, 50 m -37, 0 a 37,37 0 1,1 74,0 a 37,37 0 1,1 -74,0">
+        </path>
       </defs>
       <text font-size="17">
         <textPath xlink:href="#circle">
           <tspan>
+            find your bluebottle cafe. 당신이 원하는 블루보틀 카페를 찾아보세요.
             find your bluebottle cafe. 당신이 원하는 블루보틀 카페를 찾아보세요.
           </tspan>
         </textPath>
@@ -71,44 +88,54 @@
   </figure><!-- hj_cafes_photo -->
   <div class="hj_cafes_area">
     <a href="./cafes.php" data-num="0">
-      <img src="./HJ/img/marker_main.jpg" alt="">
-      여의도
+      <img src="./HJ/img/marker_main.jpg" alt="" class="hj_marker">
+      <img src="./HJ/img/logo/1.jpg" alt="" class="hj_logo">
+      <span>여의도</span>
     </a>
-    <a href="" data-num="1">
-      <img src="./HJ/img/marker_main.jpg" alt="">
-      명동
+    <a href="./cafes.php" data-num="1">
+      <img src="./HJ/img/marker_main.jpg" alt="" class="hj_marker">
+      <img src="./HJ/img/logo/2.jpg" alt="" class="hj_logo">
+      <span>명동</span>
     </a>
-    <a href="" data-num="2">
-      <img src="./HJ/img/marker_main.jpg" alt="">
-      광화문
+    <a href="./cafes.php" data-num="2">
+      <img src="./HJ/img/marker_main.jpg" alt="" class="hj_marker">
+      <img src="./HJ/img/logo/3.jpg" alt="" class="hj_logo">
+      <span>광화문</span>
     </a>
-    <a href="" data-num="3">
-      <img src="./HJ/img/marker_main.jpg" alt="">
-      삼청
+    <a href="./cafes.php" data-num="3">
+      <img src="./HJ/img/marker_main.jpg" alt="" class="hj_marker">
+      <img src="./HJ/img/logo/4.jpg" alt="" class="hj_logo">
+      <span>삼청</span>
     </a>
-    <a href="" data-num="4">
-      <img src="./HJ/img/marker_main.jpg" alt="">
-      삼청한옥
+    <a href="./cafes.php" data-num="4">
+      <img src="./HJ/img/marker_main.jpg" alt="" class="hj_marker">
+      <img src="./HJ/img/logo/5.jpg" alt="" class="hj_logo">
+      <span>삼청한옥</span>
     </a>
-    <a href="" data-num="5">
-      <img src="./HJ/img/marker_main.jpg" alt="">
-      한남
+    <a href="./cafes.php" data-num="5">
+      <img src="./HJ/img/marker_main.jpg" alt="" class="hj_marker">
+      <img src="./HJ/img/logo/6.jpg" alt="" class="hj_logo">
+      <span>한남</span>
     </a>
-    <a href="" data-num="6">
-      <img src="./HJ/img/marker_main.jpg" alt="">
-      성수
+    <a href="./cafes.php" data-num="6">
+      <img src="./HJ/img/marker_main.jpg" alt="" class="hj_marker">
+      <img src="./HJ/img/logo/7.jpg" alt="" class="hj_logo">
+      <span>성수</span>
     </a>
-    <a href="" data-num="7">
-      <img src="./HJ/img/marker_main.jpg" alt="">
-      역삼
+    <a href="./cafes.php" data-num="7">
+      <img src="./HJ/img/marker_main.jpg" alt="" class="hj_marker">
+      <img src="./HJ/img/logo/8.jpg" alt="" class="hj_logo">
+      <span>역삼</span>
     </a>
-    <a href="" data-num="8">
-      <img src="./HJ/img/marker_main.jpg" alt="">
-      압구정
+    <a href="./cafes.php" data-num="8">
+      <img src="./HJ/img/marker_main.jpg" alt="" class="hj_marker">
+      <img src="./HJ/img/logo/9.jpg" alt="" class="hj_logo">
+      <span>압구정</span>
     </a>
-    <a href="" data-num="9">
-      <img src="./HJ/img/marker_main.jpg" alt="">
-      제주도
+    <a href="./cafes.php" data-num="9">
+      <img src="./HJ/img/marker_main.jpg" alt="" class="hj_marker">
+      <img src="./HJ/img/logo/10.jpg" alt="" class="hj_logo">
+      <span>제주도</span>
     </a>
   </div><!-- hj_cafes_area -->
 </section><!-- hj_main_4 -->
