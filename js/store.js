6d5149bbc4a153ed10cbd964912bed90b20b100c
Choi-Mele <cikth0810@gmail.com>
2023-01-05 18:24:48 +0900

    ik section3 end

--- js/store.js
+++ js/store.js
@@ -19,26 +19,26 @@ window.backgroundArr=[
 window.main3Arr=[
   {
     title : 'espresso',
-    desc : "If a Little Dreaming is Dangerous, <br> The Cure For It is Not To Dream Less <br> But To Dream More, To Dream All the Time <br> - MARCEL PROUST -",
+    desc : "Limited-edition espresso with notes of dark chocolate, dried raspberry, and salted toffee <br><br> Our Winter Espresso is rich, complex, and velvety. It is pure luxury, and everyone deserves a moment of luxury this holiday season. A dark, brooding base is colored by bright wisps of tart fruit for a deep cup laced with nuance. This silky coffee makes for a rich experience as you linger over the at-home ritual of grinding beans, tamping grounds, pulling espresso, and savoring every last sip.",
     cup : "./img/object/main_cup04.png",
-    img : "./img/grid/bb_grid04.jpg",
+    img : "./img/grid/bb_grid18.jpg",
   },
   {
     title : 'single origin',
-    desc : "If a Little Dreaming is Dangerous, <br> The Cure For It is Not To Dream Less <br> But To Dream More, To Dream All the Time <br> - MARCEL PROUST -",
+    desc : "Ethiopia Gedeb METAD Natural <br><br> Every holiday season, we find ourselves craving this single origin. An unintentional annual feature, Ethiopia Gedeb METAD Natural calls to us when the holiday excitement is in full swing. With floral notes of honey and tart blueberry jam this naturally processed coffee boasts an overt fruitiness and complexity that dazzles and enlivens— the perfect accompaniment to all holiday gatherings.",
     cup : "./img/object/main_cup03.png",
-    img : "./img/grid/bb_grid14.jpg",
+    img : "./img/grid/bb_grid10.jpg",
   },
   {
-    title : 'blend',
-    desc : "If a Little Dreaming is Dangerous, <br> The Cure For It is Not To Dream Less <br> But To Dream More, To Dream All the Time <br> - MARCEL PROUST -",
+    title : 'winter blend',
+    desc : "Deeply fruited, limited-edition blend with notes of dark chocolate and molasses <br><br> Warm and full-bodied with a glimmer of brightness from an Ethiopian component, this blend is a beautiful embodiment of the season’s comfort. Rich notes of bittersweet chocolate and thick molasses are folded into layers of sweet and tart blackberry jam. For our Giant Steps and Bella Donovan lovers, this blend has a familiar flavor profile that’s tied up in a ribbon of fruity brightness",
     cup : "./img/object/main_cup02.png",
     img : "./img/grid/bb_grid02.jpg",
   },
   {
     title : 'cold brew',
-    desc : "If a Little Dreaming is Dangerous, <br> The Cure For It is Not To Dream Less <br> But To Dream More, To Dream All the Time <br> - MARCEL PROUST -",
+    desc : "Whole Bean Coffee Blends <br><br> We often call this trio of African coffees the single origin lover’s blend. Yet the appeal of Three Africas spans more than any one kind of coffee. Sparkling yet grounded, Three Africas is roasted dark enough to be rounded and full, yet light enough to let flecks of berries and citrus shine. It’s a coffee that gives more than what the drinker is asking for: fruit, nuance, radiance, and a deep sweetness that wins over just about everyone.",
     cup : "./img/object/main_cup01.png",
-    img : "./img/grid/bb_grid05.jpg",
+    img : "./img/grid/bb_grid14.jpg",
   },
 ]
