6574f1417fcb00d4e9ff33fa54bb4e348e551704
Choi-Mele <cikth0810@gmail.com>
2023-01-06 10:33:46 +0900

    ik main3 fix

--- js/main3.js
+++ js/main3.js
@@ -1,17 +1,22 @@
-$('.ik_main3>ul.ik_main_list_container>li>.ik_right').click(function(){
-  
-  var selectNum = parseInt($(this).attr('data-n'))
-  
-  $('.ik_main3>ul.ik_main_list_container>li>.ik_right').each(function(){
-    var rightNum = parseInt($(this).attr('data-n'))
-    
-    if(selectNum < rightNum){
-      $(this).parent().removeClass('active')
-    }else{
-      $(this).parent().toggleClass('active')
+$(function () {
+  var selectNum = null;
+  var prevNum = undefined;
+  $(".ik_main3>ul.ik_main_list_container>li>.ik_right").click(function () {
+    selectNum = parseInt($(this).attr("data-n"));
+
+    if (selectNum !== prevNum) {
+      $(this).parent().nextAll().removeClass("active");
+      $(this).parent().addClass("active");
+      $(this).parent().prevAll("li").addClass("active");
+    } else {
+      //같은 번호를 두번 클릭하면 모두 들어감
+
+      $(".ik_main3>ul.ik_main_list_container>li").removeClass("active");
     }
-  })
-  
-  $('.ik_main3>ul.ik_main_list_container>li').not($(this).parent()).removeClass('active2')
-  $(this).parent().toggleClass('active2')
-})
+    
+    prevNum = selectNum;
+
+    $(".ik_main3>ul.ik_main_list_container>li").not($(this).parent()).removeClass("active2");
+    $(this).parent().toggleClass("active2");
+  });
+});
