811bd0e6d5c8ad87b2bd80fb875a7abf19a287e4
Choi-Mele <cikth0810@gmail.com>
2023-01-08 23:34:48 +0900

    section1 section2 header fix

--- main-section1.php
+++ main-section1.php
@@ -1,10 +1,25 @@
 <section class="ik_main_1">
-  <video class="ik_background_video" src="http://cameleon89.synology.me/blue_bottle/video/bluebottle.mp4" autoplay muted loop>
-  </video>
-  <div class="ik_title_box">
-    <h2 class="ik_title">blue bottle</h2>
-    <p>Another Level of Coffee</p>
-  </div>
 
+  <div class="ik_menu">
+    <div class="ik_left">
+      <img class="ik_front" src="./img/main_section/main_left.jpg">
+      <video class="ik_back" src="http://cameleon89.synology.me/blue_bottle/video/bluebottle.mp4" autoplay muted loop>
+        
+      </video>
+    </div>
+    <div class="ik_right">
+      <img class="ik_front" src="./img/main_section/main_right.jpg">
+      <video class="ik_back" src="http://cameleon89.synology.me/blue_bottle/video/bluebottle.mp4" autoplay muted loop>
+        
+      </video>
+    </div>
+  </div>
   
 </section><!-- ik_main1 -->
+
+<script>
+  $('.ik_menu').click(function(){
+    $(this).toggleClass('active')
+  })
+
+</script>
