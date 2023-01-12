1ac8fb17b8255053dd1dcf0941b4599e6155fccc
mandarin <hoojunee@gmail.com>
2023-01-12 18:15:44 +0900

    main_section4_23.01.12
    resposive design 재설계함

--- HJ/JS/index_section4.js
+++ HJ/JS/index_section4.js
@@ -1,31 +1,31 @@
 $(function () {
+    const findPhoto = $('.hj_cafes_photo')
+    const findPhotoImg = $('.hj_cafes_photo img')
+    const findPhotoSvg = $('.hj_cafes_photo svg')
     const cafeImageArr = [
         "location_blue1", "location_blue2", "location_blue3", "location_blue4", "location_blue5",
         "location_blue6", "location_blue7", "location_blue8", "location_blue9", "location_blue10"
     ]//cafeImageArr
-    const locationNumArr = [
-        '2', '3', '4', '5', '6', '7', '8', '9', '10'
-    ]//locationImageArr
     $('.hj_ripples').ripples({
         resolution: 700,
         dropRadius: 19,
         perturbance: 0.05,
     }) //ripples
     $('.hj_ripples canvas').css({ 'border-radius': '50%' })
-    $('.hj_cafes_photo img').css({ 'display': 'none' })
+    findPhotoImg.css({ 'display': 'none' })
     $('.hj_cafes_area a').mouseenter(function () {
-        $('.hj_cafes_photo').css({ 'z-index': 3 })
-        $('.hj_cafes_photo svg').css({ 'display': 'none' })
-        $('.hj_cafes_photo img').css({ 'display': 'block' })
+        findPhoto.css({ 'z-index': 3 })
+        findPhotoSvg.css({ 'display': 'none' })
+        findPhotoImg.css({ 'display': 'block' })
         let hj_anchorData = parseInt($(this).attr('data-num'))
         if (hj_anchorData === cafeImageArr.indexOf(cafeImageArr[hj_anchorData])) {
-            $('.hj_cafes_photo img').attr("src", "./img/location/" + cafeImageArr[hj_anchorData] + ".jpg");
+            findPhotoImg.attr("src", "./img/location/" + cafeImageArr[hj_anchorData] + ".jpg");
         }//if
     })//mouseenter
     $('.hj_cafes_area').mouseleave(function () {
-        $('.hj_cafes_photo').css({ 'z-index': 1 })
-        $('.hj_cafes_photo svg').css({ 'display': 'block' })
-        $('.hj_cafes_photo img').css({ 'display': 'none' })
+        findPhoto.css({ 'z-index': 1 })
+        findPhotoSvg.css({ 'display': 'block' })
+        findPhotoImg.css({ 'display': 'none' })
     })//mouseleave
 
     $('.hj_cafes_area a').click(function () {
@@ -42,7 +42,27 @@ $(function () {
     $('.hj_cafes_area a img.hj_logo').mouseleave(function () {
         $(this).css({ 'animation': 'chScale 7s alternate infinite' })
     })//mouseenter
+
+    $('.hj_cafes_area a').bind('touchstart', function (e) {
+        e.preventDefault();
+        findPhoto.css({ 'z-index': 3 })
+        findPhotoSvg.css({ 'display': 'none' })
+        findPhotoImg.css({ 'display': 'block' })
+        let hj_anchorData = parseInt($(this).attr('data-num'))
+        if (hj_anchorData === cafeImageArr.indexOf(cafeImageArr[hj_anchorData])) {
+            findPhotoImg.attr("src", "./img/location/" + cafeImageArr[hj_anchorData] + ".jpg");
+        }//if
+    })//touchmove
     /* icon mouseenter시에 animation 없앴다가 --> mouseleave시에 animation 다시 살리기! */
+    $(window).resize(function () {
+        if ($(window).innerWidth() < 500) {
+            $('.hj_cafes_area a span').css({ 'opacity': '1' })
+        } else {
+            $('.hj_cafes_area a span').addClass('hover')
+            $('.hj_cafes_area a span').css({ 'opacity': '0' })
+        }//if-else
+    })//resize_event
+
 })//document.ready
 
 
